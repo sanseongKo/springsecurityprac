0c493a79cda61c41fdc782041cc2dbcc14a8c352
sanseongKo <tkstjd56@naver.com>
2021-06-21 20:50:18 +0900

    spring security practice 7

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.cos.security1.config;
+//1. 코드받기(인증), 2. 엑세스토큰(권한),
+//3. 사용자 프로필 정보를 가져오고 
+//4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+//4-2 (이메일, 전화번호, 이름, 아이디) 쇼핑몰 ->(집주소), 백화점 몰 ->(유저 등급필요)
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,11 +14,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)//시큐어 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	//해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
 	@Bean
 	public BCryptPasswordEncoder encodePwd() {
@@ -33,6 +43,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.defaultSuccessUrl("/")//특정페이지에서 loginForm으로 가게되면 특정페이지로 다시 보내줌	
 		.and()
 		.oauth2Login()
-		.loginPage("/loginForm");// 구글 로그인이 완료된 뒤의 후처리가 필요함
+		.loginPage("/loginForm")
+		.userInfoEndpoint()
+		.userService(principalOauth2UserService);// 구글 로그인이 완료된 뒤의 후처리가 필요함 tip. 코드X, (엑세스 토큰+사용자 프로필 정보를 받아옴)
+		
 		}
 }
