078842238f61cf8dbedec2c1b6c96abacfb234f7
sanseongKo <tkstjd56@naver.com>
2021-06-21 20:30:50 +0900

    Oauth Google Login pom.xml configuration

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -30,6 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.formLogin()
 		.loginPage("/loginForm")
 		.loginProcessingUrl("/login")// /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌.
-		.defaultSuccessUrl("/");//특정페이지에서 loginForm으로 가게되면 특정페이지로 다시 보내줌		
+		.defaultSuccessUrl("/")//특정페이지에서 loginForm으로 가게되면 특정페이지로 다시 보내줌	
+		.and()
+		.oauth2Login()
+		.loginPage("/loginForm");// 구글 로그인이 완료된 뒤의 후처리가 필요함
 		}
 }
