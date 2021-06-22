0000f6e78c64978d7f093889f3fbbb750571f76f
sanseongKo <tkstjd56@naver.com>
2021-06-22 23:55:59 +0900

    Spring Security Practice Facebook Added

--- src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security1/config/oauth/PrincipalOauth2UserService.java
@@ -9,6 +9,9 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import com.cos.security1.auth.PrincipalDetails;
+import com.cos.security1.config.oauth.provider.FacebookUserInfo;
+import com.cos.security1.config.oauth.provider.GoogleUserInfo;
+import com.cos.security1.config.oauth.provider.OAuth2UserInfo;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 
@@ -31,10 +34,20 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService{
 		//우리가 구글 로그인버튼 클릭을 하면 -> 구글 로그인 창이 뜨고 -> 로그인 완료 -> 코드 리턴(OAuth-Client라이브러리) -> AccessToken요청
 		//userRequest정보 -> loadUser 함수 호출 ->구글로부터 회원프로필 받아준다. 
 		System.out.println("userRequest3: "+super.loadUser(userRequest).getAttributes());
-				
-		String provider = userRequest.getClientRegistration().getClientId();// 구글
-		String providerId = oauth2User.getAttribute("sub");
-		String email = oauth2User.getAttribute("email");
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청");
+			oAuth2UserInfo = new GoogleUserInfo(oauth2User.getAttributes());
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("페이스북 로그인 요청");
+			oAuth2UserInfo = new FacebookUserInfo(oauth2User.getAttributes());
+		}else {
+			System.out.println("우리는 구글과 페이스북만 지원해요");
+		}
+		
+		String provider = oAuth2UserInfo.getProvider();//userRequest.getClientRegistration().getRegistrationId();
+		String providerId = oAuth2UserInfo.getProviderId();//oauth2User.getAttribute("sub");
+		String email = oAuth2UserInfo.getEmail();
 		String username = provider +"_" + providerId;
 		String password = bCryptPasswordEncoder.encode("겟인데어");
 		String role = "ROLE_USER";
